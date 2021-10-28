995e5f1a20b8485b9c762acd34dc5dac7a6a6d22
Alex Barstow <abarstow@brightcove.com>
2021-10-28 12:41:12 -0400

    update deps and default browserslist

--- es.js
+++ es.js
@@ -3,12 +3,27 @@ const transformObjectAssign = require.resolve('@babel/plugin-transform-object-as
 const transformRuntime = require.resolve('@babel/plugin-transform-runtime');
 const presetEnv = require.resolve('@babel/preset-env');
 
+const defaultBrowsersList = [
+  'last 3 major versions',
+  'Firefox ESR',
+  // This ensures support for certain smart TVs (ex. LG WebOS 4)
+  'Chrome >= 53',
+  'not dead',
+  'not ie 11',
+  'not baidu 7',
+  'not and_qq 11',
+  'not and_uc 12',
+  'not kaios 2',
+  'not op_mini all',
+  'not op_mob 64'
+];
+
 module.exports = {
   exclude: path.join(process.cwd(), 'node_modules/**'),
   compact: false,
   babelrc: false,
   presets: [
-    [presetEnv, {bugfixes: true, loose: true, modules: false, targets: {browsers: ['defaults', 'ie 11']}}]
+    [presetEnv, {bugfixes: true, loose: true, modules: false, targets: defaultBrowsersList}]
   ],
   plugins: [
     [transformRuntime, {regenerator: false}],
